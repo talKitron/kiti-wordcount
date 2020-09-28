152e259e822bd0647be7264a1a78026fa2e74e24
Tal Kitron <talkitron@gmail.com>
2020-09-28 19:41:30 +0200

    Add Config.py class for configuration settings, modify wsgi.py and app.py accordingly

--- src/app.py
+++ src/app.py
@@ -1,15 +1,25 @@
 """ Flask Application """
 
 #load libraries
+import os
 from flask import Flask
 # load modules
 from src.endpoints.wordcount import wordcount
 
 # init Flask app
 app = Flask(__name__)
+#app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 # register blueprints. ensure that all paths are versioned!
 app.register_blueprint(wordcount)
+
+@app.route('/env')
+def env():
+    if 'APP_SETTINGS' in os.environ:
+        return "Current environment: {}".format(os.environ['APP_SETTINGS'])
+    else:
+        return 'APP_SETTINGS not loaded'
 
 @app.route('/')
 def home():
@@ -19,5 +29,5 @@ def home():
 def hello_name(name):
     return "Hello {}!".format(name)
 
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 """Web Server Gateway Interface"""
-
+import os
 from src.app import app
 
 if __name__ == '__main__':
     #DEV ENV
-    app.run(host='0.0.0.0', debug=True)
+    app.config.from_object(os.environ['APP_SETTINGS'])
+    #app.run(host='0.0.0.0', debug=True)
