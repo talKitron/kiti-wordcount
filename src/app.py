2700fc4311b2564324802b3eaa38f15bad159443
Tal Kitron <talkitron@gmail.com>
2020-09-28 19:00:30 +0200

    Add Procfile for heroku deployment

--- src/app.py
+++ src/app.py
@@ -13,7 +13,11 @@ app.register_blueprint(wordcount)
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return hello_name('World')
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
