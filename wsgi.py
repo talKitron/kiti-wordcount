04cf282bca21847f82db4435e3b09f896a24e542
Tal Kitron <talkitron@gmail.com>
2020-09-29 15:54:03 +0200

    Move src/config.py to root folder, update wsgi.py accordingly

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,8 @@
 import os
 from src.app import app
 
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 if __name__ == '__main__':
     #DEV ENV
-    app.config.from_object(os.environ['APP_SETTINGS'])
-    #app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'])
